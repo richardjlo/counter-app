48d0ed6d986d9e8a469cd848ac2255c4ac846e2f
Richard Lo <richardjosonlo@gmail.com>
2018-12-01 12:54:59 +0700

    added state to handle count

--- src/index.js
+++ src/index.js
@@ -2,10 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Counter} from './components/Counter';
 
-const CounterApp = () => {
-  return(
-    <Counter count={5} />
-  );
+class CounterApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 3,
+    }
+  };
+
+  render() {
+    return(
+      <Counter count={this.state.count} />      
+    );
+  }
 };
 
 ReactDOM.render(
