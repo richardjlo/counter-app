d3b96c0d3ea4e3efdfcc977e4aa71c07e6a2b328
Richard Lo <richardjosonlo@gmail.com>
2018-12-01 13:27:23 +0700

    created event handlers for increment and decrement buttons

--- src/index.js
+++ src/index.js
@@ -8,11 +8,25 @@ class CounterApp extends React.Component {
     this.state = {
       count: 3,
     }
+    this.handleIncrementButton = this.handleIncrementButton.bind(this);
+    this.handleDecrementButton = this.handleDecrementButton.bind(this);
   };
+
+  handleIncrementButton() {
+    alert('Increment!');
+  }
+
+  handleDecrementButton() {
+    alert('Decrement');
+  }
 
   render() {
     return(
-      <Counter count={this.state.count} />      
+      <Counter 
+        count={this.state.count} 
+        onIncrement={this.handleIncrementButton}  
+        onDecrement={this.handleDecrementButton}
+      />      
     );
   }
 };
