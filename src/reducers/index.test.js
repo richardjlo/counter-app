9c1fe9333b38d78c689e7a607b8530a5a4d7b5c5
Richard Lo <richardjosonlo@gmail.com>
2018-12-02 17:39:35 +0700

    wrote first test for  increment

--- src/reducers/index.test.js
+++ src/reducers/index.test.js
@@ -1,7 +1,12 @@
 import {counterReducer} from './index';
+import {increment} from '../actions/actions';
 
 describe('counter reducer', () => {
   it('counterReducer exists', () => {
     expect(counterReducer).toBeDefined();
+  })
+
+  it('increment counter from 0 to 1', () => {
+    expect(counterReducer({ count: 0 }, increment())).toEqual({ count: 1 });
   })
 });
