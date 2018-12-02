cc8a1a361be844d411c6ad4fd59c2d3ec1aa0aec
Richard Lo <richardjosonlo@gmail.com>
2018-12-02 19:10:01 +0700

    updated handleDecrementButton to use store.dispatch instead of local state

--- src/index.js
+++ src/index.js
@@ -5,48 +5,34 @@ import {counterReducer} from './reducers/index';
 import { increment, decrement } from './actions/actions';
 import { createStore } from 'redux'
 
-const store = createStore(counterReducer);
-
-console.log('count ' + store.getState().count);
-
 class CounterApp extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       count: 0,
     }
-    // this.handleIncrementButton = this.handleIncrementButton.bind(this);
-    // this.handleDecrementButton = this.handleDecrementButton.bind(this);
   };
-
-  // handleIncrementButton() {
-  //   this.setState({
-  //     count: this.state.count + 1,
-  //   });
-  // }
-
-  // handleDecrementButton() {
-  //   this.setState({
-  //     count: this.state.count - 1,
-  //   });
-  // }
 
   handleIncrementButton() {
     store.dispatch(increment());
     // alert(store.getState().count);
   }  
+  handleDecrementButton() {
+    store.dispatch(decrement());
+  }
 
   render() {
     return(
       <Counter 
-        // count={this.state.count} 
         count={store.getState().count}
         onIncrement={this.handleIncrementButton}  
-        // onDecrement={this.handleDecrementButton}
+        onDecrement={this.handleDecrementButton}
       />      
     );
   }
 };
+
+const store = createStore(counterReducer);
 
 const render = () => {
   ReactDOM.render(
