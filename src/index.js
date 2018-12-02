03957f58cbfdf516f5f2cb110d5ce6bb7dffe0f8
Richard Lo <richardjosonlo@gmail.com>
2018-12-02 19:07:56 +0700

    updated handleIncrementButton to use store.dispatch instead of local state. Renders updates using store.subscribe

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Counter} from './components/Counter';
+import {counterReducer} from './reducers/index';
+import { increment, decrement } from './actions/actions';
+import { createStore } from 'redux'
+
+const store = createStore(counterReducer);
+
+console.log('count ' + store.getState().count);
 
 class CounterApp extends React.Component {
   constructor(props) {
@@ -8,34 +15,45 @@ class CounterApp extends React.Component {
     this.state = {
       count: 0,
     }
-    this.handleIncrementButton = this.handleIncrementButton.bind(this);
-    this.handleDecrementButton = this.handleDecrementButton.bind(this);
+    // this.handleIncrementButton = this.handleIncrementButton.bind(this);
+    // this.handleDecrementButton = this.handleDecrementButton.bind(this);
   };
 
-  handleIncrementButton() {
-    this.setState({
-      count: this.state.count + 1,
-    });
-  }
+  // handleIncrementButton() {
+  //   this.setState({
+  //     count: this.state.count + 1,
+  //   });
+  // }
 
-  handleDecrementButton() {
-    this.setState({
-      count: this.state.count - 1,
-    });
-  }
+  // handleDecrementButton() {
+  //   this.setState({
+  //     count: this.state.count - 1,
+  //   });
+  // }
+
+  handleIncrementButton() {
+    store.dispatch(increment());
+    // alert(store.getState().count);
+  }  
 
   render() {
     return(
       <Counter 
-        count={this.state.count} 
+        // count={this.state.count} 
+        count={store.getState().count}
         onIncrement={this.handleIncrementButton}  
-        onDecrement={this.handleDecrementButton}
+        // onDecrement={this.handleDecrementButton}
       />      
     );
   }
 };
 
-ReactDOM.render(
-  <CounterApp />, 
-  document.getElementById('root')
-);
+const render = () => {
+  ReactDOM.render(
+    <CounterApp />, 
+    document.getElementById('root')
+  );
+};
+
+render();
+store.subscribe(render);
