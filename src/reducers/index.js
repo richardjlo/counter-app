a0ad8d378be37392ed4e43905833a5a4d02434e3
Richard Lo <richardjosonlo@gmail.com>
2018-12-02 17:49:52 +0700

    added tests for type:'unknown', initial state

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,10 +2,16 @@
  * Counter Reducer
  */ 
 
-export const counterReducer = (state, action) => {
+export const counterReducer = (state = { count: 0 }, action) => {
   if(action.type === 'INCREMENT') {
     return({
       count: state.count + 1,
     });
+  } else if(action.type === 'DECREMENT') {
+    return({
+      count: state.count -1,
+    });
+  } else {
+    return state;
   }
 };
--- src/reducers/index.test.js
+++ src/reducers/index.test.js
@@ -1,12 +1,24 @@
 import {counterReducer} from './index';
-import {increment} from '../actions/actions';
+import {increment, decrement} from '../actions/actions';
 
 describe('counter reducer', () => {
-  it('counterReducer exists', () => {
+  it('should return true if counterReducer exists', () => {
     expect(counterReducer).toBeDefined();
   })
 
-  it('increment counter from 0 to 1', () => {
-    expect(counterReducer({ count: 0 }, increment())).toEqual({ count: 1 });
+  it('should provide initial state to be count = 0', () => {
+    expect(counterReducer(undefined, {})).toEqual({count: 0});
+  })
+
+  it('should increment counter from 0 to 1', () => {
+    expect(counterReducer( { count: 0 }, increment())  ).toEqual({ count: 1 });
+  })
+
+  it('should decrement counter from 5 to 4', () => {
+    expect(counterReducer( { count: 5 }, decrement() )).toEqual({ count: 4});
+  })
+
+  it('should return same state if passing undefined action', () => {
+    expect(counterReducer( { count: 2 }, { type: 'unkown' } )).toEqual({ count: 2});
   })
 });
