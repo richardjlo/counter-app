c52128f35e077c4a69fe855988a69f9f9f2941a0
Richard Lo <richardjosonlo@gmail.com>
2018-12-03 12:16:41 +0700

    added connect function

--- src/containers/CounterApp.js
+++ src/containers/CounterApp.js
@@ -3,12 +3,19 @@ import { Counter } from '../components/Counter';
 import { increment, decrement } from '../actions/actions';
 import { connect } from "react-redux";
 
-export const CounterApp = () => {
+const CounterApp = () => {
   return(
     <Counter 
+      count={111}
       // count={store.getState().count}
       // onIncrement={() => {store.dispatch(increment())}}
       // onDecrement={() => {store.dispatch(decrement())}}
     />      
   );
 };
+
+function mapStateToProps(state) {
+  return state.count;
+}
+
+export default connect(mapStateToProps)(CounterApp);
