68bffbc219012b999242a9aef8eb7db44b7fc364
Richard Lo <richardjosonlo@gmail.com>
2018-12-06 20:02:02 +0700

    removed commented out code

--- src/index.js
+++ src/index.js
@@ -1,24 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import { counterReducer } from './reducers/index';
 import { addCounterReducer } from './reducers/addCounterReducer';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-// import CounterApp from './containers/CounterApp';
 import CounterAppMultiple from './containers/CounterAppMultiple';
 
-// const store = createStore(counterReducer);
 const store = createStore(addCounterReducer);
 
 const render = () => {
   ReactDOM.render(
-    // <Provider store={store}>
-    //   <CounterApp />
-    // </Provider>, 
     <Provider store={store}>
       <CounterAppMultiple />
     </Provider>,
-    // <CounterAppMultiple countersList={[0,1,2]}/>,
     document.getElementById('root')
   );
 };
