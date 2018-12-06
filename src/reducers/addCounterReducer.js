a1d7cc0609b181e8ca37d47023b2fdcb178c9675
Richard Lo <richardjosonlo@gmail.com>
2018-12-06 17:31:29 +0700

    renamed defaultState to initialState, updated if, then to switch statement

--- src/reducers/addCounterReducer.js
+++ src/reducers/addCounterReducer.js
@@ -1,10 +1,11 @@
-const defaultState = [];
+const initialState = [];
 
-export const addCounterReducer = (state = defaultState, action) => {
-  if(action.type === 'ADD_COUNTER') {
-    const newState = state.concat(0);
-    return newState;
-  } else {
-    return defaultState;
-  }  
+export const addCounterReducer = (state = initialState, action) => {
+  switch(action.type) { 
+    case 'ADD_COUNTER':
+      const newState = state.concat(0);
+      return newState;
+    default:
+      return state;
+  }
 };
