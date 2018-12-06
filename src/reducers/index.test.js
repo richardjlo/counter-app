a23660922620ff5ab8f752938681c5f97bd24c87
Richard Lo <richardjosonlo@gmail.com>
2018-12-06 20:14:38 +0700

    updated test file

--- src/reducers/index.test.js
+++ src/reducers/index.test.js
@@ -1,38 +1,42 @@
-import {addCounterReducer} from './index';
+import {counterReducer} from './index'
 import { addCounter, increment, decrement } from '../actions/index';
 
 describe('addCounterReducer', () => {
   it('should return true if addCounterReducer exists', () => {
-    expect(addCounterReducer).toBeDefined();
+    expect(counterReducer).toBeDefined();
   }) 
 
   it('should provide initial state to be counters = []', () => {
-    expect(addCounterReducer(undefined, {})).toEqual([]);
+    expect(counterReducer(undefined, {})).toEqual([]);
+  })  
+
+  it('should return state if action is unknown', () => {
+    expect(counterReducer([], {})).toEqual([]);
   })  
 
   const prevState = [];
   Object.freeze(prevState);
   it('should add counter from [] to [0]', () => {
-    expect(addCounterReducer( prevState, addCounter()) ).toEqual([0]);
+    expect(counterReducer( prevState, addCounter()) ).toEqual([0]);
   })
 
   it('should add counter from 1 counter ([0]) to 2 counters ([0, 0])', () => {
-    expect(addCounterReducer( [0], addCounter()) ).toEqual([0, 0]);
+    expect(counterReducer( [0], addCounter()) ).toEqual([0, 0]);
   })
 
   it('should add counter from 2 counters ([0, 0]) to 3 counters ([0, 0, 0])', () => {
-    expect(addCounterReducer( [0, 0], addCounter()) ).toEqual([0, 0, 0]);
+    expect(counterReducer( [0, 0], addCounter()) ).toEqual([0, 0, 0]);
   })
 
   const prevState1 = [0];
   Object.freeze(prevState1);
   it('should increment counter @ 0 from 0 to 1', () => {
-    expect(addCounterReducer( prevState1, increment(0)) ).toEqual([1]);
+    expect(counterReducer( prevState1, increment(0)) ).toEqual([1]);
   })  
 
   const prevState2 = [2];
   Object.freeze(prevState2);
   it('should decrement counter @ 0 from 2 to 1', () => {
-    expect(addCounterReducer( prevState2, decrement(0)) ).toEqual([1]);
+    expect(counterReducer( prevState2, decrement(0)) ).toEqual([1]);
   })    
 });
