f4a3dd7849cf6282dfb60a0dd1dbfbe23cb9bd3d
Richard Lo <richardjosonlo@gmail.com>
2018-12-06 17:54:17 +0700

    added logic for increment in reducer

--- src/reducers/addCounterReducer.js
+++ src/reducers/addCounterReducer.js
@@ -5,6 +5,11 @@ export const addCounterReducer = (state = initialState, action) => {
     case 'ADD_COUNTER':
       const newState = state.concat(0);
       return newState;
+    case 'INCREMENT': 
+      let newState2 = state.slice();
+      let value = newState2[action.index];
+      newState2[action.index] = value + 1;
+      return(newState2);
     default:
       return state;
   }
