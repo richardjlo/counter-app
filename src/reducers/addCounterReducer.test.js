15cf3aa1b9ff7fb353bfb79ddc862fb403ad72dd
Richard Lo <richardjosonlo@gmail.com>
2018-12-06 18:02:00 +0700

    updated test file to test for decrement

--- src/reducers/addCounterReducer.test.js
+++ src/reducers/addCounterReducer.test.js
@@ -1,5 +1,5 @@
 import {addCounterReducer} from './addCounterReducer';
-import { addCounter, increment } from '../actions/index';
+import { addCounter, increment, decrement } from '../actions/index';
 
 describe('addCounterReducer', () => {
   it('should return true if addCounterReducer exists', () => {
@@ -27,12 +27,12 @@ describe('addCounterReducer', () => {
   const prevState1 = [0];
   Object.freeze(prevState1);
   it('should increment counter @ 0 from 0 to 1', () => {
-    expect(addCounterReducer( prevState1, increment(0))  ).toEqual([1]);
+    expect(addCounterReducer( prevState1, increment(0)) ).toEqual([1]);
   })  
 
   const prevState2 = [2];
   Object.freeze(prevState2);
-  it('should decrementt counter @ 0 from 0 to 1', () => {
-    expect(addCounterReducer( prevState2, decrement(0))  ).toEqual([1]);
+  it('should decrement counter @ 0 from 2 to 1', () => {
+    expect(addCounterReducer( prevState2, decrement(0)) ).toEqual([1]);
   })    
 });
