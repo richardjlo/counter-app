7a176abc76ae221aa85fe2bbf0bae548728424ef
Richard Lo <richardjosonlo@gmail.com>
2018-12-06 15:37:35 +0700

    added test for adding a single counter

--- src/reducers/addCounterReducer.test.js
+++ src/reducers/addCounterReducer.test.js
@@ -1,4 +1,5 @@
 import {addCounterReducer} from './addCounterReducer';
+import { addCounter } from '../actions/index';
 
 describe('addCounterReducer', () => {
   it('should return true if addCounterReducer exists', () => {
@@ -8,4 +9,8 @@ describe('addCounterReducer', () => {
   it('should provide initial state to be counters = []', () => {
     expect(addCounterReducer(undefined, {})).toEqual([]);
   })  
+
+  it('should add counter from [] to [0]', () => {
+    expect(addCounterReducer( [], addCounter())  ).toEqual([0]);
+  })
 });
