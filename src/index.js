44d20932900ff659e58e09611d144ed6f25d9c08
Richard Lo <richardjosonlo@gmail.com>
2018-12-06 17:30:43 +0700

    created store using counter app multiple reducer

--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { counterReducer } from './reducers/index';
+// import { counterReducer } from './reducers/index';
+import { addCounterReducer } from './reducers/addCounterReducer';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 // import CounterApp from './containers/CounterApp';
-import { CounterAppMultiple } from './components/CounterAppMultiple';
+import CounterAppMultiple from './containers/CounterAppMultiple';
 
-const store = createStore(counterReducer);
+// const store = createStore(counterReducer);
+const store = createStore(addCounterReducer);
 
 const render = () => {
   ReactDOM.render(
     // <Provider store={store}>
     //   <CounterApp />
     // </Provider>, 
-    <CounterAppMultiple countersList={[0,0]} />,
+    <Provider store={store}>
+      <CounterAppMultiple />
+    </Provider>,
+    // <CounterAppMultiple countersList={[0,1,2]}/>,
     document.getElementById('root')
   );
 };
