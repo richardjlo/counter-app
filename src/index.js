271b0989a795343dda0fab15f4f459c6fcbc7927
Richard Lo <richardjosonlo@gmail.com>
2018-12-06 16:54:47 +0700

    temporarily commented out original counterapp

--- src/index.js
+++ src/index.js
@@ -3,15 +3,17 @@ import ReactDOM from 'react-dom';
 import { counterReducer } from './reducers/index';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import CounterApp from './containers/CounterApp';
+// import CounterApp from './containers/CounterApp';
+import { CounterAppMultiple } from './components/CounterAppMultiple';
 
 const store = createStore(counterReducer);
 
 const render = () => {
   ReactDOM.render(
-    <Provider store={store}>
-      <CounterApp />
-    </Provider>, 
+    // <Provider store={store}>
+    //   <CounterApp />
+    // </Provider>, 
+    <CounterAppMultiple countersList={[0,0]} />,
     document.getElementById('root')
   );
 };
