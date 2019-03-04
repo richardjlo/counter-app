45db647dd3a0354e4e153ccfe51ef07a9ebee61f
Richard Lo <richardjosonlo@gmail.com>
2019-03-04 10:18:58 +0800

    updated formatting of import statements

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { counterReducer } from './reducers';
 import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
+import { thunk } from 'redux-thunk';
 import { Provider } from 'react-redux';
-import {CounterAppContainer} from './containers/CounterAppContainer';
+import { CounterAppContainer } from './containers/CounterAppContainer';
 
 const store = createStore(
   counterReducer,
