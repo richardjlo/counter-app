bb40d1ecd3f08533bf6933bf9b01299340ff45e4
Richard Lo <richardjosonlo@gmail.com>
2019-03-18 16:15:04 +0800

    commented out original code

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,38 +2,38 @@
  * Action Creators - addCounter, increment, decrement
  */
 
-export const addCounter = () => {
-  return({
-    type: 'ADD_COUNTER',
-  });  
-};
+// export const addCounter = () => {
+//   return({
+//     type: 'ADD_COUNTER',
+//   });  
+// };
 
-export const deleteCounter = (i) => {
-  return({
-    index: i,
-    type: 'DELETE_COUNTER',
-  });
-};
+// export const deleteCounter = (i) => {
+//   return({
+//     index: i,
+//     type: 'DELETE_COUNTER',
+//   });
+// };
 
-export const increment = (i) => {
-  return({
-    index: i,
-    type: 'INCREMENT',
-  });  
-};
+// export const increment = (i) => {
+//   return({
+//     index: i,
+//     type: 'INCREMENT',
+//   });  
+// };
 
-export const asyncIncrement = (i) => {
-  return dispatch => {
-    setTimeout(() => {
-      // Yay! Can invoke sync or async actions with `dispatch`
-      dispatch(increment(i));
-    }, 1000);
-  };
-};
+// export const asyncIncrement = (i) => {
+//   return dispatch => {
+//     setTimeout(() => {
+//       // Yay! Can invoke sync or async actions with `dispatch`
+//       dispatch(increment(i));
+//     }, 1000);
+//   };
+// };
 
-export const decrement = (i) => {
-  return({
-    index: i,
-    type: 'DECREMENT',
-  });  
-};
+// export const decrement = (i) => {
+//   return({
+//     index: i,
+//     type: 'DECREMENT',
+//   });  
+// };
