cc001e2d42f992575cb18b7bdcadf525fee297a9
Richard Lo <richardjosonlo@gmail.com>
2019-03-18 16:08:06 +0800

    added reducer and store back to index.js

--- src/index.js
+++ src/index.js
@@ -6,49 +6,18 @@ import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import { CounterAppContainer } from './containers/CounterAppContainer';
 
-import { CounterApp } from './components/CounterApp';
-
-// const store = createStore(
-//   counterReducer,
-//   applyMiddleware(thunk)
-// );
-
-// const render = () => {
-//   ReactDOM.render(
-//     <Provider store={store}>
-//       <CounterAppContainer />
-//     </Provider>,
-//     document.getElementById('root')
-//   );
-// };
-
-// render();
-
-const counter1_id = 'b9mY8KQy2p4FIb7MJ5LP';
-const counter2_id = 'uB3LUWoA8tsdxDzTq0Py';
-const counter3_id = 'uB3LUWoA8tsdxDzTq3xm';
-
-const countersList = {
-  isFetching: false, 
-  counters: {
-    [counter1_id]: {
-      value: 1,
-    },
-    [counter2_id]: {
-      value: 2,
-    },
-    [counter3_id]: {
-      value: 4,
-    }
-  }
-};
+const store = createStore(
+  counterReducer,
+  applyMiddleware(thunk)
+);
 
 const render = () => {
   ReactDOM.render(
-    <CounterApp countersList={countersList}/>,
+    <Provider store={store}>
+      <CounterAppContainer />
+    </Provider>,
     document.getElementById('root')
   );
 };
 
 render();
-
