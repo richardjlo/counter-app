8aa82d14e4dd2619cb07e06040e646612d0bf3ab
Richard Lo <richardjosonlo@gmail.com>
2019-03-18 16:16:28 +0800

    added firebase and createCounterRequest, createCounterSuccess, createCounter action creators

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,60 @@
  * Action Creators - addCounter, increment, decrement
  */
 
+import firebase from "firebase";
+
+var config = {
+  apiKey: "AIzaSyB1zMXq5j4DzjntJNyajFgaoM90t3sFXcY",
+  authDomain: "counter-app-d343f.firebaseapp.com",
+  databaseURL: "https://counter-app-d343f.firebaseio.com",
+  projectId: "counter-app-d343f",
+  storageBucket: "counter-app-d343f.appspot.com",
+  messagingSenderId: "451958929152"
+};
+
+firebase.initializeApp(config);
+const db = firebase.firestore();
+const countersRef = db.collection("counters");
+
+export const CREATE_COUNTER = 'CREATE_COUNTER';
+
+export const createCounterRequest = () => {
+  return({
+    type: CREATE_COUNTER,
+  });
+};
+
+export const createCounterSuccess = (response) => {
+  return({
+    type: CREATE_COUNTER,
+    status: 'success',
+    response: response,
+  });
+};
+
+export const createCounter = () => {
+  const createCounterDispatchFunction = (dispatch) => {
+    // Toggle isFetching flag ON.
+    dispatch(createCounterRequest());  
+    
+    // Add a new document in collection "counters"
+    countersRef.add({
+      value: 0,
+      created: firebase.firestore.Timestamp.now().seconds,
+    })
+    .then(function(docRef) {
+      console.log("Document written with ID: ", docRef.id);
+      // TODO: Send document id to addCounter action creator
+      dispatch( createCounterSuccess(docRef.id) );
+    })
+    .catch(function(error) {
+      console.error("Error writing document: ", error);
+    });    
+  }
+
+  return createCounterDispatchFunction;
+};
+
 // export const addCounter = () => {
 //   return({
 //     type: 'ADD_COUNTER',
