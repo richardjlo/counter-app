b4cd0010daf4e47d06c23e12a2d0db8075ceee4f
Richard Lo <richardjosonlo@gmail.com>
2019-03-18 16:01:48 +0800

    updated counters object to get values from an object instead of array

--- src/components/CounterApp.js
+++ src/components/CounterApp.js
@@ -8,21 +8,32 @@ import { Counter } from './Counter';
 export const CounterApp = (props) => {
   const { 
     countersList, 
-    handleAddCounterButton, 
-    handleIncrement, 
-    handleDecrement, 
-    handleDeleteCounterButton,
-    handleAsyncIncrement,
+    // handleAddCounterButton, 
+    // handleIncrement, 
+    // handleDecrement, 
+    // handleDeleteCounterButton,
+    // handleAsyncIncrement,
   } = props;
-  const counters = countersList.map( (value, index) => {
-    return (
+  // const counters = countersList.map( (value, index) => {
+  //   return (
+  //     <Counter
+  //       key={index}
+  //       count={value}
+  //       onIncrement={() => {handleIncrement(index)}}
+  //       onDecrement={() => {handleDecrement(index)}}
+  //       onDelete={() => {handleDeleteCounterButton(index)}}
+  //       onAsyncIncrement={() => {handleAsyncIncrement(index)}}
+  //     />
+  //   );
+  // });
+
+  const countersArr = Object.keys(countersList.counters); // Get counters key into a separate array object
+  const counters = countersArr.map( (key) => {
+    let counter = countersList.counters[key];
+    return (      
       <Counter
-        key={index}
-        count={value}
-        onIncrement={() => {handleIncrement(index)}}
-        onDecrement={() => {handleDecrement(index)}}
-        onDelete={() => {handleDeleteCounterButton(index)}}
-        onAsyncIncrement={() => {handleAsyncIncrement(index)}}
+        key={key}
+        count={counter.value}
       />
     );
   });
@@ -32,7 +43,7 @@ export const CounterApp = (props) => {
       {counters}
       <hr />
       <button
-        onClick={() => {handleAddCounterButton()}}
+        // onClick={() => {handleAddCounterButton()}}
       >
         Add Counter
       </button>
