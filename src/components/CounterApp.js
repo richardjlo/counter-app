f62d94f8bdfc94550e5a66379850a78fbce090bc
Richard Lo <richardjosonlo@gmail.com>
2019-03-19 10:14:57 +0800

    converted CounterApp to class

--- src/components/CounterApp.js
+++ src/components/CounterApp.js
@@ -5,48 +5,50 @@ import { Counter } from './Counter';
  * Presentational component that adds one or more counters to the DOM
  */
 
-export const CounterApp = (props) => {
-  const { 
-    countersList, 
-    handleAddCounterButton, 
-    // handleIncrement, 
-    // handleDecrement, 
-    // handleDeleteCounterButton,
-    // handleAsyncIncrement,
-  } = props;
-  // const counters = countersList.map( (value, index) => {
-  //   return (
-  //     <Counter
-  //       key={index}
-  //       count={value}
-  //       onIncrement={() => {handleIncrement(index)}}
-  //       onDecrement={() => {handleDecrement(index)}}
-  //       onDelete={() => {handleDeleteCounterButton(index)}}
-  //       onAsyncIncrement={() => {handleAsyncIncrement(index)}}
-  //     />
-  //   );
-  // });
+export class CounterApp extends React.Component {
+  render() {
+    const { 
+      countersList, 
+      handleAddCounterButton, 
+      // handleIncrement, 
+      // handleDecrement, 
+      // handleDeleteCounterButton,
+      // handleAsyncIncrement,
+    } = this.props;
+    // const counters = countersList.map( (value, index) => {
+    //   return (
+    //     <Counter
+    //       key={index}
+    //       count={value}
+    //       onIncrement={() => {handleIncrement(index)}}
+    //       onDecrement={() => {handleDecrement(index)}}
+    //       onDelete={() => {handleDeleteCounterButton(index)}}
+    //       onAsyncIncrement={() => {handleAsyncIncrement(index)}}
+    //     />
+    //   );
+    // });
 
-  const countersArr = Object.keys(countersList.counters); // Get counters key into a separate array object
-  const counters = countersArr.map( (key) => {
-    let counter = countersList.counters[key];
-    return (      
-      <Counter
-        key={key}
-        count={counter.value}
-      />
-    );
-  });
+    const countersArr = Object.keys(countersList.counters); // Get counters key into a separate array object
+    const counters = countersArr.map( (key) => {
+      let counter = countersList.counters[key];
+      return (      
+        <Counter
+          key={key}
+          count={counter.value}
+        />
+      );
+    });
 
-  return(
-    <div>
-      {counters}
-      <hr />
-      <button
-        onClick={() => {handleAddCounterButton()}}
-      >
-        Add Counter
-      </button>
-    </div>    
-  );
+    return(
+      <div>
+        {counters}
+        <hr />
+        <button
+          onClick={() => {handleAddCounterButton()}}
+        >
+          Add Counter
+        </button>
+      </div>    
+    );    
+  }
 };
