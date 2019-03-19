0ebc471404e03c530ec4bef8101258719421f885
Richard Lo <richardjosonlo@gmail.com>
2019-03-19 11:19:33 +0800

    updating createCounterSuccess to accept parameters for id and object

--- src/reducers/index.test.js
+++ src/reducers/index.test.js
@@ -7,24 +7,6 @@ import {
 } from '../actions/index';
 
 describe('Counter Reducer', () => {
-  const counter1 = {
-    id: 'b9mY8KQy2p4FIb7MJ5LP',
-    created: 1552892019,
-    value: 0,
-  };
-
-  const counter2 = {
-    id: 'b9mY8KQy2p4FIb7MJ5LQ',
-    created: 1552892020,
-    value: 1,
-  };
-
-  const counter3 = {
-    id: 'b9mY8KQy2p4FIb7MJ5LX',
-    created: 1552892021,
-    value: 2,
-  };
-  
   const initialState = {
     isFetching: false,
     counters: {},
@@ -69,61 +51,76 @@ describe('createCounterSuccess action creator', () => {
   };
   
   Object.freeze(initialState);
-  const counter1_id = 'b9mY8KQy2p4FIb7MJ5LP';
-  const counter2_id = 'uB3LUWoA8tsdxDzTq0Py';
-  const counter3_id = 'uB3LUWoA8tsdxDzTq3xm';
+  // const counter1_id = 'b9mY8KQy2p4FIb7MJ5LP';
+  // const counter2_id = 'uB3LUWoA8tsdxDzTq0Py';
+  // const counter3_id = 'uB3LUWoA8tsdxDzTq3xm';
+  const b9mY8KQy2p4FIb7MJ5LP = {
+    created: 1552892019,
+    value: 0,
+  };
+
+  const b9mY8KQy2p4FIb7MJ5LQ = {
+    created: 1552892020,
+    value: 1,
+  };
+
+  const b9mY8KQy2p4FIb7MJ5LX = {
+    created: 1552892021,
+    value: 2,
+  };
 
   const oneCounter = {
     isFetching: false,
     counters: {
-      [counter1_id]: {
+      b9mY8KQy2p4FIb7MJ5LP: {
+        created: 1552892019,
         value: 0,
-      }
-    }        
-  };
-
-  it('should add counter to empty counters list.', () => {
-    expect(counterReducer( initialState, createCounterSuccess(counter1_id)) )
-      .toEqual(oneCounter);
-  });
-
-  Object.freeze(oneCounter);
-
-  const twoCounters = {
-    isFetching: false, 
-    counters: {
-      [counter1_id]: {
-        value: 0,
-      },
-      [counter2_id]: {
-        value: 0,
-      }
-    }     
-  };
-  
-  it('should add counter from 1 counter to 2 counters', () => {
-    expect(counterReducer( oneCounter, createCounterSuccess(counter2_id)) ).toEqual(twoCounters);
-  });
-
-  Object.freeze(twoCounters);
-  const threeCounters = {
-    isFetching: false, 
-    counters: {
-      [counter1_id]: {
-        value: 0,
-      },
-      [counter2_id]: {
-        value: 0,
-      },
-      [counter3_id]: {
-        value: 0,
-      }
+      }        
     }
   };
 
-  it('should add counter from 2 counters to 3 counters', () => {
-    expect(counterReducer( twoCounters, createCounterSuccess(counter3_id)) ).toEqual(threeCounters);
-  });    
+  it('should add counter to empty counters list.', () => {
+    expect(counterReducer( initialState, createCounterSuccess('b9mY8KQy2p4FIb7MJ5LP', b9mY8KQy2p4FIb7MJ5LP)) )
+      .toEqual(oneCounter);
+  });
+
+  // Object.freeze(oneCounter);
+
+  // const twoCounters = {
+  //   isFetching: false, 
+  //   counters: {
+  //     [counter1_id]: {
+  //       value: 0,
+  //     },
+  //     [counter2_id]: {
+  //       value: 0,
+  //     }
+  //   }     
+  // };
+  
+  // it('should add counter from 1 counter to 2 counters', () => {
+  //   expect(counterReducer( oneCounter, createCounterSuccess(counter2_id)) ).toEqual(twoCounters);
+  // });
+
+  // Object.freeze(twoCounters);
+  // const threeCounters = {
+  //   isFetching: false, 
+  //   counters: {
+  //     [counter1_id]: {
+  //       value: 0,
+  //     },
+  //     [counter2_id]: {
+  //       value: 0,
+  //     },
+  //     [counter3_id]: {
+  //       value: 0,
+  //     }
+  //   }
+  // };
+
+  // it('should add counter from 2 counters to 3 counters', () => {
+  //   expect(counterReducer( twoCounters, createCounterSuccess(counter3_id)) ).toEqual(threeCounters);
+  // });    
 });
 
 describe('fetchCountersRequest action creator', () => {
