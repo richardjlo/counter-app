c0bae0d4a9c66fcab8dd139cd9dadf617eb60a2a
Richard Lo <richardjosonlo@gmail.com>
2019-03-20 13:57:16 +0800

    removed comments

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -98,30 +98,3 @@ export const counterReducer = (state = initialState, action) => {
       return state;
   }  
 };
-
-
-// export const counterReducer = (state = initialState, action) => {
-//   switch(action.type) { 
-//     case 'ADD_COUNTER':
-//       return [...state, 0];     // same as `return state.concat(0);`
-//     case 'INCREMENT': 
-//       return [
-//         ...state.slice(0, action.index),    // Part of array before index
-//         state[action.index] + 1,            // Incremented value at index
-//         ...state.slice(action.index + 1),   // Part of array after index
-//       ]
-//     case 'DECREMENT':
-//     return [
-//       ...state.slice(0, action.index),    // Part of array before index
-//       state[action.index] - 1,            // Decremented value at index
-//       ...state.slice(action.index + 1),   // Part of array after index
-//     ]    
-//     case 'DELETE_COUNTER':
-//       return [
-//         ...state.slice(0, action.index),  // Get part of array before index
-//         ...state.slice(action.index + 1)    // Get part of array afert index
-//       ];
-//     default:
-//       return state;
-//   }
-// };
