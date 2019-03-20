745b27f91b538ca5536762d298908c623ba1cdcd
Richard Lo <richardjosonlo@gmail.com>
2019-03-20 15:48:02 +0800

    added firebaseConfig file

---
+++ src/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB1zMXq5j4DzjntJNyajFgaoM90t3sFXcY",
+  authDomain: "counter-app-d343f.firebaseapp.com",
+  databaseURL: "https://counter-app-d343f.firebaseio.com",
+  projectId: "counter-app-d343f",
+  storageBucket: "counter-app-d343f.appspot.com",
+  messagingSenderId: "451958929152"
+};
+
+export default firebaseConfig;
